4f71227b1dd0db07cfc9deadbae4a42df4c35a36
Goodness Lazarus <goodnesslazarus56@gmail.com>
2026-05-19 14:13:29 +0100

    correct google verification format

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,19 @@
-export const metadata = {
-  title: "Nalis Pharmaceutical",
-  description: "Online pharmacy.",
-};
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
-        <meta
-          name="google-site-verification"
-          content="hkqcx7Bfmo1QvJO"
-        />
-      </head>
-
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
+export const metadata = {
+  title: "Nalis Pharmaceutical",
+  description: "Online pharmacy.",
+  verification: {
+    "Google site verification": "hkqcx7Bfmo1QvJO",
+  },
+};
