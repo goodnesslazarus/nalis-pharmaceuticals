e0bf51bc0d00bc9baf49d601f9c8318d4152c3d5
Goodness Lazarus <goodnesslazarus56@gmail.com>
2026-05-19 15:45:24 +0100

    fix build error (geist font)

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${classname.variable} ${classname.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         {children}
@@ -13,7 +13,5 @@ export default function RootLayout({ children }) {
 export const metadata = {
   title: "Nalis Pharmaceutical",
   description: "Online pharmacy.",
-  verification: {
-    "Google site verification": "hkqcx7Bfmo1QvJO",
-  },
-};
+  verification: {"Google site verification": "hkqcx7Bfmo1QvJO"},
+  }
