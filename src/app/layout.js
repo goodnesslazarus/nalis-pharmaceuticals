77d1fceb4dd41fd4d9ee15022c119fb7fd4c017c
Goodness Lazarus <goodnesslazarus56@gmail.com>
2026-05-19 14:00:32 +0100

    fix google verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Nalis Pharmaceutical",
   description: "Online pharmacy.",
-  verification: {
-    "Google site verification": "hkqcx7Bfmo1QvJO",
-  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta
+          name="google-site-verification"
+          content="hkqcx7Bfmo1QvJO"
+        />
+      </head>
+
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
